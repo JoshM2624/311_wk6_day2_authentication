a2c65b1da48e385f55ed1646968072f84ae26184
Joshua Martinez <jam@Joshuas-MacBook-Pro.local>
2019-09-06 09:53:18 -0500

    pushing work test 1

--- sql/connection.js
+++ sql/connection.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql')
-
+ 
 class Connection {
   constructor() {
     if (!this.pool) {
       console.log('creating mysql connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: '35.184.42.200',
+        host: '104.154.68.251',
         user: 'root',
-        password: 'password',
+        password: 'purple',
         database: 'admin'
       })
 
